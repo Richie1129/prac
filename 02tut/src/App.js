a9b930e3123b9f7ccaa216db91268f04740e34bf
0932283947a@gmail.com <63162885+Richie1129@users.noreply.github.com>
2023-03-03 17:18:47 +0800

    1st commit

---
+++ 02tut/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
